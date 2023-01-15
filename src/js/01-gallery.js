699da258366a0f44c4c4e8ac2fed2dd54e633d20
Iryna Popova <popovaira969@gmail.com>
2023-01-15 23:21:25 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+console.log(galleryEl);
+
+function createGalleryImages(galleryItems) {
+  return galleryItems
+    .map(
+      ({ original, preview, description }) => `
+      <a class="gallery__item"  href=${original}>
+  <img class="gallery__image" src=${preview} alt="${description}" />
+</a>`
+    )
+    .join('');
+}
+const createGallery = createGalleryImages(galleryItems);
+
+galleryEl.innerHTML = createGallery;
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+console.log(gallery);
+
+
