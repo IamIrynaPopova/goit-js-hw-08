2417f18e2eda4d3866bb4c12807132b7ac46229d
Iryna Popova <popovaira969@gmail.com>
2023-01-16 23:50:40 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import Lodash from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const getlocalStorageValue = localStorage.getItem(LOCALSTORAGE_KEY);
+const timeValue = JSON.parse(getlocalStorageValue);
+
+const onPlay = function (data) {
+    console.log(data);
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
+};
+player.on('timeupdate', onPlay);
+player.setCurrentTime(timeValue.seconds);
